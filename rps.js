cd82d22835ea476be312b5be5be7a893430b78a6
Lladnar-Fall <lutuciri90@gmail.com>
2024-06-04 22:44:49 +0400

    Created function to play round and added it to another function to play the game for 5 rounds

--- rps.js
+++ rps.js
@@ -17,5 +17,42 @@ function getComputerChoice() {
 }
 
 
-const humanScore = 0
-const computerScore = 0
+const humanScore = 0;
+const computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice.toLowerCase() === "rock" && computerChoice == "scissors") {
+        return alert("You win!! rock beats scissors");
+        ++humanScore
+    } else if (humanChoice.toLowerCase() === "rock" && computerChoice == "rock") {
+        return alert("Draw both picked rock");
+    }
+    else if (humanChoice.toLowerCase() === "paper" && computerChoice == "rock") {
+        return alert("You win!! paper smothers rock");
+        ++humanScore
+    } else if (humanChoice.toLowerCase() === "paper" && computerChoice == "paper") {
+        return alert("Draw both picked paper");
+    }
+    else if (humanChoice.toLowerCase() === "scissors" && computerChoice == "paper") {
+        return alert("You win!! scissors cuts paper");
+        ++humanScore;
+    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice == "scissors") {
+        return alert("Draw both pick scissors")
+    }
+    else {
+        return (`You lose, you picked ${humanChoice} and comp picked ${computerChoice}`);
+        ++humanScore;
+    }
+
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = getHumanChoice();
+        const compChoice = getComputerChoice();
+
+        playRound(playerChoice,compChoice);
+    }
+    humanScore > computerScore ? alert(`Player won the game!🥳🥳, Player points: ${humanScore} Computer points: ${computerScore}`) : alert(`Computer won the game!🥳🥳, Computer points: ${computerScore} Player points: ${humanScore}`);
+}
+
